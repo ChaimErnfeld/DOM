ad3dccddcb367e413f1b724e10da89dd26613de5
ChaimErnfeld <mp058328@gmail.com>
2024-09-11 11:55:21 +0300

    finish

--- CaeRace/script.js
+++ CaeRace/script.js
@@ -1,29 +1,35 @@
+const arr = [];
+const MAX_AMOUNT_OF_CARS = 4;
+
 const raceDiv = document.getElementsByClassName("raceDiv")[0];
 const inputCars = document.getElementById("inputCars");
 
 function addedDiv(num) {
-  for (let i = 0; i < num; i++) {
-    const recaer = document.createElement("div");
-    recaer.id = `racer${i + 1}`;
-    raceDiv.appendChild(recaer);
+  for (let i = 1; i <= num; i++) {
+    const racer = document.createElement("div");
+    racer.id = `racer${i}`;
+    raceDiv.appendChild(racer);
 
     const image = document.createElement("img");
-    image.src = `/images/car${i + 1}.png`;
-    image.id = `img${i + 1}`;
-    recaer.appendChild(image);
+    image.src = `/images/car${i}.png`;
+    image.id = `img${i}`;
+    racer.appendChild(image);
 
     const redStripe = document.createElement("div");
     redStripe.className = "redStripe";
-    recaer.appendChild(redStripe);
+    racer.appendChild(redStripe);
 
     moveCars(i);
   }
 }
 
 document.getElementById("startBtn").addEventListener("click", () => {
-  for (let i = 1; i <= 4; i++) {
-    if (document.getElementById(`racer${i}`)) {
-      document.getElementById(`racer${i}`).remove();
+  for (let i = 1; i <= MAX_AMOUNT_OF_CARS; i++) {
+    const carElement = document.getElementById(`racer${i}`);
+    const res = document.getElementById(i);
+    if (carElement) {
+      carElement.remove();
+      res.remove();
     }
   }
   addedDiv(inputCars.value);
@@ -31,14 +37,37 @@ document.getElementById("startBtn").addEventListener("click", () => {
 
 function moveCars(imgId) {
   let marginLeft = 0;
+  const startTime = Date.now();
 
   const intervalID = setInterval(() => {
     const racer = document.getElementById(`img${imgId + 1}`);
-    marginLeft += Math.random() * 100;
-    racer.style.marginLeft = `${marginLeft}px`;
+    marginLeft += Math.random() * 4;
+    racer.style.marginLeft = `${marginLeft}%`;
 
-    if (marginLeft > 1100) {
+    if (marginLeft > 94) {
       clearInterval(intervalID);
+      const endTime = Date.now();
+      const timeTaken = (endTime - startTime) / 1000;
+      printResult(imgId + 1, timeTaken);
     }
-  }, 500);
+  }, 200);
 }
+
+function printResult(carId, timeTaken) {
+  const divResult = document.getElementById("results");
+  let p = document.createElement("p");
+  p.textContent = `car ${carId} has finished in ${timeTaken}!`;
+  p.id = carId;
+  divResult.appendChild(p);
+}
+
+document.getElementById("resetBtn").addEventListener("click", () => {
+  for (let i = 1; i <= 4; i++) {
+    const carElement = document.getElementById(`racer${i}`);
+    const res = document.getElementById(i);
+    if (carElement) {
+      carElement.remove();
+      res.remove();
+    }
+  }
+});
